537078bb9dea075a9cfff8c6b9dc31e34beefa64
TCO46 <patohru2006@gmail.com>
2024-03-09 22:50:36 +0700

    Update prisma schema

--- src/express/routers/v1/Users/index.ts
+++ src/express/routers/v1/Users/index.ts
@@ -1,35 +1,36 @@
-import express from 'express'
-const router = express.Router()
-import prisma from '../../../../utils/databases/prisma'
+import express from "express";
+const router = express.Router();
+import prisma from "../../../../utils/databases/prisma";
 router.get("/@me", (req, res) => {
-    return res.json({
-        code: 200,
-        msgCode: 'a-u-200',
-        user: req.user
-    })
-})
+	return res.json({
+		code: 200,
+		msgCode: "a-u-200",
+		user: req.user,
+	});
+});
 
-router.get('/:id', async(req,res) => {
-    const user = await prisma.users?.findFirst({
-        where: {
-            id: req.params.id
-        },
-    })
-    if(!user) return res.status(404).json({
-        code: 404,
-        msgCode: 'a-u-404'
-    });
-    return res.json({
-        code: 200,
-        msg: 'a-u-200',
-        user: {
-            id: user.id,
-            username: user.username,
-            email: user.email,
-            create_at: user.create_at,
-            flags: user.flags
-        }
-    })
-})
+router.get("/:id", async (req, res) => {
+	const user = await prisma.users?.findFirst({
+		where: {
+			id: req.params.id,
+		},
+	});
+	if (!user)
+		return res.status(404).json({
+			code: 404,
+			msgCode: "a-u-404",
+		});
+	return res.json({
+		code: 200,
+		msg: "a-u-200",
+		user: {
+			id: user.id,
+			username: user.username,
+			email: user.email,
+			created_at: user.created_at,
+			flags: user.flags,
+		},
+	});
+});
 
-export default router
+export default router;
