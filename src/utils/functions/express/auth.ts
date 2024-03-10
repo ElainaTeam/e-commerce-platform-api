6c07be5732200cc4ef0c3b0c3df1f30e606527e7
Peter Tuan Anh <petertuananh@hotmail.com>
2024-03-10 09:08:45 +0000

    Auth fixed

--- src/utils/functions/express/auth.ts
+++ src/utils/functions/express/auth.ts
@@ -1,71 +1,74 @@
 import prisma from '../../../utils/databases/prisma'
+import functions from '..';
 export = {
     // This stuff will ensure that logged in to a valid credential
-    ensureAuthenticated: async function(req: any, res: any, next: any) {
+    checkUserPermitOnGlobal: async function (data: any) {
+        const user : any = await prisma.users.findFirst({
+            where: {
+                id: data.user_id,
+            }
+        });
+		const flags = JSON.parse("[" + user?.flags.replace('[', '').replace(']', '').replaceAll(`'`, `"`).replaceAll('`', `"`) + "]");
+        if (flags.includes(data.scope)) return {result: true};
+        return {result: false};
+    },
+    checkUserPermitOnShop: async function (data: any) {
+        const findShop : any = await prisma.shops.findFirst({
+            where: {
+                id: data.shop_id
+            }
+        });
+        if (!findShop) return {result: false};
+        const userShopPermit : any = await prisma.shop_permissions.findFirst({
+            where: {
+                user_id: data.user_id,
+                shop_id: data.shop_id
+            }
+        });
+		const flags = JSON.parse("[" + userShopPermit?.flags.replace('[', '').replace(']', '').replaceAll(`'`, `"`).replaceAll('`', `"`) + "]");
+        if (flags.includes(data.scope)) return {result: true};
+        return {result: false};
+    },
+    ensureAuthenticated: async function (req: any, res: any, next: any) {
         if (!req.user) return res.json({code: 401, msgCode: 'a-401'});
-        return next(req);
+        return next();
     },
     ensureUserIsGlobalAdministrator: async function(req: any, res: any, next: any) {
-        if (!req.user || !req.user.flags.includes('admin')) return res.json({code: 403, msgCode: 'a-403'});
-        return next(req);
+        if (!req.user) return res.json({code: 403, msgCode: 'a-403'});
+        const checkIfUserHaveGlobalAdmin = await functions.express.auth.checkUserPermitOnGlobal({user_id: req.user.id, scope: 'admin'});
+        if (!checkIfUserHaveGlobalAdmin.result) return res.json({code: 403, msgCode: 'a-403'});
+        return next();
     },
     ensureUserIsGlobalModerator: async function(req: any, res: any, next: any) {
-        if (!req.user || (!req.user.flags.includes('mod') && !req.user.flags.includes('admin'))) return res.json({code: 403, msgCode: 'a-403'});
-        return next(req);
+        if (!req.user) return res.json({code: 403, msgCode: 'a-403'});
+        const checkIfUserHaveGlobalAdmin = await functions.express.auth.checkUserPermitOnGlobal({user_id: req.user.id, scope: 'admin'});
+        const checkIfUserHaveGlobalMod = await functions.express.auth.checkUserPermitOnGlobal({user_id: req.user.id, scope: 'mod'});
+        if (!checkIfUserHaveGlobalAdmin.result && !checkIfUserHaveGlobalMod.result) return res.json({code: 403, msgCode: 'a-403'});
+        return next();
     },
     ensureUserIsShopOwner: async function(req: any, res: any, next: any) {
         if (!req.user) return res.json({code: 403, msgCode: 'a-403'});
-        const findShop : any = await prisma.shops.findFirst({
-            where: {
-                id: req.params.shop_id
-            }
-        });
-        if (!findShop) return res.json({code: 404, msgCode: 'a-s-404'});
-        const userShopPermit : any = await prisma.shop_permissions.findFirst({
-            where: {
-                user_id: req.user.id,
-                shop_id: req.params.shop_id
-            }
-        });
-		const flags = JSON.parse("[" + userShopPermit?.flags.replace('[', '').replace(']', '').replaceAll(`'`, `"`).replaceAll('`', `"`) + "]");
-        if (!req.user.flags.includes('admin') && !flags.includes('owner')) return res.json({code: 403, msgCode: 'a-403'});
-        return next(req);
+        const checkIfUserHaveGlobalAdmin = await functions.express.auth.checkUserPermitOnGlobal({user_id: req.user.id, scope: 'admin'});
+        const checkIfUserHaveShopOwner = await functions.express.auth.checkUserPermitOnShop({user_id: req.user.id, shop_id: req.params.shop_id, scope: 'owner'});
+        if (!checkIfUserHaveGlobalAdmin.result && !checkIfUserHaveShopOwner.result) return res.json({code: 403, msgCode: 'a-403'});
+        return next();
     },
     ensureUserIsShopAdministrator: async function(req: any, res: any, next: any) {
         if (!req.user) return res.json({code: 403, msgCode: 'a-403'});
-        const findShop : any = await prisma.shops.findFirst({
-            where: {
-                id: req.params.shop_id
-            }
-        });
-        if (!findShop) return res.json({code: 404, msgCode: 'a-s-404'});
-        const userShopPermit : any = await prisma.shop_permissions.findFirst({
-            where: {
-                user_id: req.user.id,
-                shop_id: req.params.shop_id
-            }
-        });
-		const flags = JSON.parse("[" + userShopPermit?.flags.replace('[', '').replace(']', '').replaceAll(`'`, `"`).replaceAll('`', `"`) + "]");
-        if (!req.user.flags.includes('admin') && !flags.includes('owner') && !flags.includes('admin')) return res.json({code: 403, msgCode: 'a-403'});
-        return next(req);
+        const checkIfUserHaveGlobalAdmin = await functions.express.auth.checkUserPermitOnGlobal({user_id: req.user.id, scope: 'admin'});
+        const checkIfUserHaveShopOwner = await functions.express.auth.checkUserPermitOnShop({user_id: req.user.id, shop_id: req.params.shop_id, scope: 'owner'});
+        const checkIfUserHaveShopAdmin = await functions.express.auth.checkUserPermitOnShop({user_id: req.user.id, shop_id: req.params.shop_id, scope: 'admin'});
+        if (!checkIfUserHaveGlobalAdmin.result && !checkIfUserHaveShopOwner.result && !checkIfUserHaveShopAdmin.result) return res.json({code: 403, msgCode: 'a-403'});
+        return next();
     },
     ensureUserIsShopModerator: async function(req: any, res: any, next: any) {
         if (!req.user) return res.json({code: 403, msgCode: 'a-403'});
-        const findShop : any = await prisma.shops.findFirst({
-            where: {
-                id: req.params.shop_id
-            }
-        });
-        if (!findShop) return res.json({code: 404, msgCode: 'a-s-404'});
-        const userShopPermit : any = await prisma.shop_permissions.findFirst({
-            where: {
-                user_id: req.user.id,
-                shop_id: req.params.shop_id
-            }
-        });
-		const flags = JSON.parse("[" + userShopPermit?.flags.replace('[', '').replace(']', '').replaceAll(`'`, `"`).replaceAll('`', `"`) + "]");
-        if (!req.user.flags.includes('admin') && !flags.includes('owner') && !flags.includes('admin') && !flags.includes('mod')) return res.json({code: 403, msgCode: 'a-403'});
-        return next(req);
+        const checkIfUserHaveGlobalAdmin = await functions.express.auth.checkUserPermitOnGlobal({user_id: req.user.id, scope: 'admin'});
+        const checkIfUserHaveShopOwner = await functions.express.auth.checkUserPermitOnShop({user_id: req.user.id, shop_id: req.params.shop_id, scope: 'owner'});
+        const checkIfUserHaveShopAdmin = await functions.express.auth.checkUserPermitOnShop({user_id: req.user.id, shop_id: req.params.shop_id, scope: 'admin'});
+        const checkIfUserHaveShopMod = await functions.express.auth.checkUserPermitOnShop({user_id: req.user.id, shop_id: req.params.shop_id, scope: 'mod'});
+        if (!checkIfUserHaveGlobalAdmin.result && !checkIfUserHaveShopOwner.result && !checkIfUserHaveShopAdmin.result && !checkIfUserHaveShopMod.result) return res.json({code: 403, msgCode: 'a-403'});
+        return next();
     },
     forwardAuthenticated: async function() {
         
