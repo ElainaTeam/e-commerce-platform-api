727b1485e439e5df7edf84238579a789eebf8781
Peter Tuan Anh <petertuananh@hotmail.com>
2024-03-11 14:33:10 +0000

    Fixed bugs in middle auth and package.json

--- src/middleware/Auth.ts
+++ src/middleware/Auth.ts
@@ -9,26 +9,30 @@ export default class Auth {
 
 		if (!authHeader || !token) return next();
 
-		jwt.verify(token, String(process.env.JWT_ACCESS_SECRET), async (err: any, userToken: any) => {
-			if (err || !userToken) return next(err);
-			const user : any = await prisma.users.findFirst({
-				where: {
-					id: String(userToken.id)
-				}
+		try {
+			jwt.verify(token, String(process.env.JWT_ACCESS_SECRET), async (err: any, userToken: any) => {
+				if (err || !userToken) return next(err);
+				const user : any = await prisma.users.findFirst({
+					where: {
+						id: String(userToken.id)
+					}
+				});
+				if (!user) return next(err);
+				const userSession : any = await prisma.user_sessions?.findFirst({
+					where: {
+						user_id: userToken.id,
+						access_token: token
+					}
+				});
+				if (!userSession || (userSession.expire_at < Date.now())) return next(err);
+				req.user = user;
+				const flags = JSON.parse("[" + user.flags.replace('[', '').replace(']', '').replaceAll(`'`, `"`).replaceAll('`', `"`) + "]");
+				req.user.flags = flags;
+				req.user.access_token = token
+				next();
 			});
-			if (!user) return next(err);
-			const userSession : any = await prisma.user_sessions?.findFirst({
-				where: {
-					user_id: userToken.id,
-					access_token: token
-				}
-			});
-			if (!userSession || (userSession.expire_at < Date.now())) return next(err);
-			req.user = user;
-			const flags = JSON.parse("[" + user.flags.replace('[', '').replace(']', '').replaceAll(`'`, `"`).replaceAll('`', `"`) + "]");
-			req.user.flags = flags;
-			req.user.access_token = token
-			next();
-		});
+		} catch (error) {
+			next(error);
+		}
 	}
 }
