bf2b23116f876956b4581921bb73f5ae46bcb723
Peter Tuan Anh <petertuananh@hotmail.com>
2024-03-18 15:52:49 +0000

    prisma update

--- src/express/routers/v1/Chats/index.ts
+++ src/express/routers/v1/Chats/index.ts
@@ -1,5 +1,5 @@
-import prisma from "@/utils/databases/prisma";
-import functions from "@/utils/functions";
+import prisma from "../../../../utils/databases/prisma";
+import functions from "../../../../utils/functions";
 import express from "express";
 const router = express.Router();
 
@@ -7,7 +7,15 @@ router.put("/", async (req, res) => {
     switch (req.body.type) {
         case 'user':
             if (!req.body.user_id) return res.json({code: 200, msgCode: 'a-c-400'});
-
+            const chat_id = functions.system.createSnowflakeId();
+            await prisma.chats.create({
+                data: {
+                    id: chat_id,
+                    create_at: String(Date.now()),
+                    type: req.body.type
+                }
+            });
+            return res.json({code: 200, msgCode: 'a-c-200', chat_id});
             break
         case 'group':
             if (!req.body.name) return res.json({code: 200, msgCode: 'a-c-400'});
