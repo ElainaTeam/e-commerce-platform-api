2492050e3c5f34935b075dcede5ff09811657cc7
Peter Tuan Anh <petertuananh@hotmail.com>
2024-03-20 14:48:17 +0000

    fixed no shop found act as global administrator

--- src/utils/functions/express/auth.ts
+++ src/utils/functions/express/auth.ts
@@ -8,6 +8,14 @@ export = {
                 id: data.user_id,
             }
         });
+        if (data.req.params.shop_id) {
+            const findShop : any = await prisma.shops.findFirst({
+                where: {
+                    id: data.req.params.shop_id
+                }
+            });
+            if (!findShop) return {result: false};
+        }
         if (user.flags.includes(data.scope)) return {result: true};
         return {result: false};
     },
@@ -24,8 +32,8 @@ export = {
                 shop_id: data.shop_id
             }
         });
-		const flags = JSON.parse("[" + userShopPermit?.flags.replace('[', '').replace(']', '').replaceAll(`'`, `"`).replaceAll('`', `"`) + "]");
-        if (flags.includes(data.scope)) return {result: true};
+        console.log(userShopPermit.flags)
+        if (userShopPermit.flags.includes(data.scope)) return {result: true};
         return {result: false};
     },
     ensureAuthenticated: async function (req: any, res: any, next: any) {
@@ -34,27 +42,27 @@ export = {
     },
     ensureUserIsGlobalAdministrator: async function(req: any, res: any, next: any) {
         if (!req.user) return res.json({code: 403, msgCode: 'a-403'});
-        const checkIfUserHaveGlobalAdmin = await functions.express.auth.checkUserPermitOnGlobal({user_id: req.user.id, scope: 'admin'});
+        const checkIfUserHaveGlobalAdmin = await functions.express.auth.checkUserPermitOnGlobal({req, user_id: req.user.id, scope: 'admin'});
         if (!checkIfUserHaveGlobalAdmin.result) return res.json({code: 403, msgCode: 'a-403'});
         return next();
     },
     ensureUserIsGlobalModerator: async function(req: any, res: any, next: any) {
         if (!req.user) return res.json({code: 403, msgCode: 'a-403'});
-        const checkIfUserHaveGlobalAdmin = await functions.express.auth.checkUserPermitOnGlobal({user_id: req.user.id, scope: 'admin'});
-        const checkIfUserHaveGlobalMod = await functions.express.auth.checkUserPermitOnGlobal({user_id: req.user.id, scope: 'mod'});
+        const checkIfUserHaveGlobalAdmin = await functions.express.auth.checkUserPermitOnGlobal({req, user_id: req.user.id, scope: 'admin'});
+        const checkIfUserHaveGlobalMod = await functions.express.auth.checkUserPermitOnGlobal({req, user_id: req.user.id, scope: 'mod'});
         if (!checkIfUserHaveGlobalAdmin.result && !checkIfUserHaveGlobalMod.result) return res.json({code: 403, msgCode: 'a-403'});
         return next();
     },
     ensureUserIsShopOwner: async function(req: any, res: any, next: any) {
         if (!req.user) return res.json({code: 403, msgCode: 'a-403'});
-        const checkIfUserHaveGlobalAdmin = await functions.express.auth.checkUserPermitOnGlobal({user_id: req.user.id, scope: 'admin'});
+        const checkIfUserHaveGlobalAdmin = await functions.express.auth.checkUserPermitOnGlobal({req, user_id: req.user.id, scope: 'admin'});
         const checkIfUserHaveShopOwner = await functions.express.auth.checkUserPermitOnShop({user_id: req.user.id, shop_id: req.params.shop_id, scope: 'owner'});
         if (!checkIfUserHaveGlobalAdmin.result && !checkIfUserHaveShopOwner.result) return res.json({code: 403, msgCode: 'a-403'});
         return next();
     },
     ensureUserIsShopAdministrator: async function(req: any, res: any, next: any) {
         if (!req.user) return res.json({code: 403, msgCode: 'a-403'});
-        const checkIfUserHaveGlobalAdmin = await functions.express.auth.checkUserPermitOnGlobal({user_id: req.user.id, scope: 'admin'});
+        const checkIfUserHaveGlobalAdmin = await functions.express.auth.checkUserPermitOnGlobal({req, user_id: req.user.id, scope: 'admin'});
         const checkIfUserHaveShopOwner = await functions.express.auth.checkUserPermitOnShop({user_id: req.user.id, shop_id: req.params.shop_id, scope: 'owner'});
         const checkIfUserHaveShopAdmin = await functions.express.auth.checkUserPermitOnShop({user_id: req.user.id, shop_id: req.params.shop_id, scope: 'admin'});
         if (!checkIfUserHaveGlobalAdmin.result && !checkIfUserHaveShopOwner.result && !checkIfUserHaveShopAdmin.result) return res.json({code: 403, msgCode: 'a-403'});
@@ -62,7 +70,7 @@ export = {
     },
     ensureUserIsShopModerator: async function(req: any, res: any, next: any) {
         if (!req.user) return res.json({code: 403, msgCode: 'a-403'});
-        const checkIfUserHaveGlobalAdmin = await functions.express.auth.checkUserPermitOnGlobal({user_id: req.user.id, scope: 'admin'});
+        const checkIfUserHaveGlobalAdmin = await functions.express.auth.checkUserPermitOnGlobal({req, user_id: req.user.id, scope: 'admin'});
         const checkIfUserHaveShopOwner = await functions.express.auth.checkUserPermitOnShop({user_id: req.user.id, shop_id: req.params.shop_id, scope: 'owner'});
         const checkIfUserHaveShopAdmin = await functions.express.auth.checkUserPermitOnShop({user_id: req.user.id, shop_id: req.params.shop_id, scope: 'admin'});
         const checkIfUserHaveShopMod = await functions.express.auth.checkUserPermitOnShop({user_id: req.user.id, shop_id: req.params.shop_id, scope: 'mod'});
