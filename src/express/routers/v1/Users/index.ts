ef0ae2c8000064ba889b840100f65793c49590d2
TCO46 <patohru2006@gmail.com>
2024-05-30 11:24:24 +0700

    [feat] Add user Reactions, Posts

--- src/express/routers/v1/Users/index.ts
+++ src/express/routers/v1/Users/index.ts
@@ -2,17 +2,66 @@ import express from "express";
 const router = express.Router();
 import prisma from "../../../../utils/databases/prisma";
 router.get("/@me", (req, res) => {
-	function exclude(user : any, keys : any) {
-		return Object.fromEntries(
-		  Object.entries(user).filter(([key]) => !keys.includes(key))
-		);
-	}
+	// function exclude(user : any, keys : any) {
+	// 	return Object.fromEntries(
+	// 	  Object.entries(user).filter(([key]) => !keys.includes(key))
+	// 	);
+	// }
 	return res.json({
 		code: 200,
 		msgCode: "a-u-200",
-		user: exclude(req.user, ['hashed_password', 'access_token']),
+		// user: exclude(req.user, ['access_token']),
+		user: req.user
 	});
 });
+
+router.get("/@me/posts", async (req, res) => {
+	const getPost = await prisma.forum_post.findMany({ 
+		where: {
+			user_id: req.user.id
+		}
+	})
+
+	return res.json({
+		code: 200,
+		msgCode: "a-u-200",
+		posts: getPost
+	})
+})
+
+router.get("/@me/reactions", async (req, res) => {
+	const getPostReaction = await prisma.forum_post_reaction.findMany({
+		where: {
+			user_id: req.user.id
+		},
+		include: {
+			post: true,
+		}
+	})
+
+	return res.json({
+		code: 200,
+		msgCode: "a-u-200",
+		reactions: getPostReaction
+	})
+})
+
+router.get("/@me/reactions/comment", async (req, res) => {
+	const getCommentReaction = await prisma.forum_post_comment_reaction.findMany({
+		where: {
+			user_id: req.user.id
+		},
+		include: {
+			comment: true,
+		}
+	})
+
+	return res.json({
+		code: 200,
+		msgCode: "a-u-200",
+		reactions: getCommentReaction
+	})
+})
 
 router.get("/:id", async (req, res) => {
 	const user = await prisma.users?.findFirst({
